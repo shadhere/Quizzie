46859b63c66555b6cb14e5a3f3611ccff8f64992
shadhere <93948067+shadhere@users.noreply.github.com>
2024-02-03 18:04:28 +0530

    s

--- server/index.js
+++ server/index.js
@@ -13,15 +13,8 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
 const attemptRoute = require("./routes/attemptRoute");
 
-const corsOptions = {
-  origin: "*", // or specify the allowed origins
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
