608bfff9b3cf49de24ee0db5f6c70b053c7ff1b6
shadhere <93948067+shadhere@users.noreply.github.com>
2024-02-03 17:24:23 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -14,7 +14,22 @@ const analyticsRoutes = require("./routes/analyticsRoutes");
 const attemptRoute = require("./routes/attemptRoute");
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "https://quizzie-2s1d.vercel.app/",
+  "http://localhost:4000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
